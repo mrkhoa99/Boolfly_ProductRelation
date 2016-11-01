79a45974483753cbcfb76d38a0d7516b59b62861
Mr Khoa <mrkhoa99@gmail.com>
2016-11-01 17:31:29 +0700

    Updated Magento 2.1.2

--- Boolfly/ProductRelation/Model/Product/Initialization/Helper/ProductLinks.php
+++ Boolfly/ProductRelation/Model/Product/Initialization/Helper/ProductLinks.php
@@ -2,31 +2,120 @@
 
 namespace Boolfly\ProductRelation\Model\Product\Initialization\Helper;
 
+use Boolfly\ProductRelation\Model\Catalog\Product\Link;
+use Magento\Catalog\Api\Data\ProductLinkExtensionFactory;
+use Magento\Catalog\Api\Data\ProductLinkInterfaceFactory;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+
 class ProductLinks
 {
     /**
-     * @var \Magento\Backend\Helper\Js
+     * String name for link type
      */
-    protected $jsHelper;
+    const TYPE_NAME = 'customtype';
+    /**
+     * @var ProductLinkInterfaceFactory
+     */
+    protected $productLinkFactory;
 
+    /**
+     * @var ProductRepositoryInterface
+     */
+    protected $productRepository;
+
+    /**
+     * @var ProductLinkExtensionFactory
+     */
+    protected $productLinkExtensionFactory;
+
+    /**
+     * Init
+     *
+     * @param ProductLinkInterfaceFactory $productLinkFactory
+     * @param ProductRepositoryInterface $productRepository
+     * @param ProductLinkExtensionFactory $productLinkExtensionFactory
+     */
     public function __construct(
-        \Magento\Backend\Helper\Js $jsHelper
-    )
-    {
-        $this->jsHelper = $jsHelper;
+        ProductLinkInterfaceFactory $productLinkFactory,
+        ProductRepositoryInterface $productRepository,
+        ProductLinkExtensionFactory $productLinkExtensionFactory
+    ) {
+        $this->productLinkFactory = $productLinkFactory;
+        $this->productRepository = $productRepository;
+        $this->productLinkExtensionFactory = $productLinkExtensionFactory;
     }
 
-    public function aroundInitializeLinks(
-        $subject,
-        \Closure $proceed,
-        $product,
+    public function beforeInitializeLinks(
+        \Magento\Catalog\Model\Product\Initialization\Helper\ProductLinks $subject,
+        \Magento\Catalog\Model\Product $product,
         array $links
     )
     {
-        if (isset($links['customtype']) && !$product->getCustomtypeReadonly()) {
-            //Need to decode the value from grid.
-            $product->setCustomtypeData($this->jsHelper->decodeGridSerializedInput($links['customtype']));
+        if(isset($links[self::TYPE_NAME]) && !$product->getCustomtypeReadonly()) {
+
+            $links = (isset($links[self::TYPE_NAME])) ? $links[self::TYPE_NAME] : $product->getCustomtypeLinkData();
+            if (!is_array($links)) {
+                $links = [];
+            }
+
+            if ($product->getCustomtypeLinkData()) {
+                $links = array_merge($links, $product->getCustomtypeLinkData());
+            }
+            $newLinks = [];
+            $existingLinks = $product->getProductLinks();
+            foreach ($links as $linkRaw) {
+                /** @var \Magento\Catalog\Api\Data\ProductLinkInterface $productLink */
+                $productLink = $this->productLinkFactory->create();
+                if (!isset($linkRaw['id'])) {
+                    continue;
+                }
+                $productId = $linkRaw['id'];
+                if (!isset($linkRaw['qty'])) {
+                    $linkRaw['qty'] = 0;
+                }
+                $linkedProduct = $this->productRepository->getById($productId);
+
+                $productLink->setSku($product->getSku())
+                    ->setLinkType(self::TYPE_NAME)
+                    ->setLinkedProductSku($linkedProduct->getSku())
+                    ->setLinkedProductType($linkedProduct->getTypeId())
+                    ->setPosition($linkRaw['position'])
+                    ->getExtensionAttributes()
+                    ->setQty($linkRaw['qty']);
+
+                $newLinks[] = $productLink;
+            }
+
+            $existingLinks = $this->removeUnExistingLinks($existingLinks, $newLinks);
+            $product->setProductLinks(array_merge($existingLinks, $newLinks));
         }
-        return $proceed($product, $links);
     }
+
+    /**
+     * Removes unexisting links
+     *
+     * @param array $existingLinks
+     * @param array $newLinks
+     * @return array
+     */
+    private function removeUnExistingLinks($existingLinks, $newLinks)
+    {
+        $result = [];
+        foreach ($existingLinks as $key => $link) {
+            $result[$key] = $link;
+            if ($link->getLinkType() == self::TYPE_NAME) {
+                $exists = false;
+                foreach ($newLinks as $newLink) {
+                    if ($link->getLinkedProductSku() == $newLink->getLinkedProductSku()) {
+                        $exists = true;
+                    }
+                }
+                if (!$exists) {
+                    unset($result[$key]);
+                }
+            }
+        }
+        return $result;
+    }
+
 }
---
+++ Boolfly/ProductRelation/Model/Product/Link/CollectionProvider/CustomType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Boolfly\ProductRelation\Model\Product\Link\CollectionProvider;
+
+class CustomType implements \Magento\Catalog\Model\ProductLink\CollectionProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getLinkedProducts(\Magento\Catalog\Model\Product $product)
+    {
+        return $product->getTypeInstance()->getAssociatedProducts($product);
+    }
+}
